2b50185ea16ad33828bb9a0174c4a682e3a44879
Elun <tupieurods@gmail.com>
2017-08-24 23:41:14 +0700

    Base AES implementation.

--- CryptoExperiments/AES/main.cpp
+++ CryptoExperiments/AES/main.cpp
@@ -12,17 +12,72 @@ AESparam AES128param = { 4, 4, 10 };
 AESparam AES192param = { 6, 4, 12 };
 AESparam AES256param = { 8, 4, 14 };
 
+#define BYTE_N(x, n) ((x >> 8 * n) & 0x000000FFU)
+#define BYTE0(x) BYTE_N(x, 0)
+#define BYTE1(x) BYTE_N(x, 1)
+#define BYTE2(x) BYTE_N(x, 2)
+#define BYTE3(x) BYTE_N(x, 3)
+
+// xtime is a macro that finds the product of {02} and the argument to
+// xtime modulo {1b}
+#define XTIME(x) ((x<<1) ^ (((x>>7) & 1) * 0x1b))
+#define X3(x) (x ^ XTIME(x))
+
+unsigned int constructUint(unsigned char b3, unsigned char b2, unsigned char b1, unsigned char b0)
+{
+  return b3 << 24 & 0xFF000000U
+    | b2 << 16 & 0x00FF0000U
+    | b1 << 8 & 0x0000FF00U
+    | b0 & 0x000000FFU;
+}
+
 unsigned int SubWord(unsigned int value)
 {
-  return sboxTable[value >> 24 & 0x000000FFU] << 24 & 0xFF000000U
-    | sboxTable[value >> 16 & 0x000000FFU] << 16 & 0x00FF0000U
-    | sboxTable[value >> 8 & 0x000000FFU] << 8 & 0x0000FF00U
-    | sboxTable[value & 0x000000FFU] & 0x000000FFU;
+  return constructUint(sboxTable[BYTE3(value)], sboxTable[BYTE2(value)], sboxTable[BYTE1(value)], sboxTable[BYTE0(value)]);
 }
 
 unsigned int RotWord(unsigned value)
 {
   return value << 8 | value >> 24;
+}
+
+void AddRoundKey(unsigned int *state, unsigned int *w)
+{
+  state[0] ^= w[0];
+  state[1] ^= w[1];
+  state[2] ^= w[2];
+  state[3] ^= w[3];
+}
+
+void ShiftRows(unsigned int *state)
+{
+  unsigned int tmp[4];
+  tmp[0] = constructUint(BYTE3(state[0]), BYTE2(state[1]), BYTE1(state[2]), BYTE0(state[3]));
+  tmp[1] = constructUint(BYTE3(state[1]), BYTE2(state[2]), BYTE1(state[3]), BYTE0(state[0]));
+  tmp[2] = constructUint(BYTE3(state[2]), BYTE2(state[3]), BYTE1(state[0]), BYTE0(state[1]));
+  tmp[3] = constructUint(BYTE3(state[3]), BYTE2(state[0]), BYTE1(state[1]), BYTE0(state[2]));
+  state[0] = tmp[0];
+  state[1] = tmp[1];
+  state[2] = tmp[2];
+  state[3] = tmp[3];
+}
+
+unsigned int MixColumn(unsigned int value)
+{
+  return constructUint(
+    XTIME(BYTE3(value)) ^ X3(BYTE2(value)) ^ BYTE1(value) ^ BYTE0(value),
+    BYTE3(value) ^ XTIME(BYTE2(value)) ^ X3(BYTE1(value)) ^ BYTE0(value),
+    BYTE3(value) ^ BYTE2(value) ^ XTIME(BYTE1(value)) ^ X3(BYTE0(value)),
+    X3(BYTE3(value)) ^ BYTE2(value) ^ BYTE1(value) ^ XTIME(BYTE0(value))
+  );
+}
+
+void MixColumns(unsigned int *state)
+{
+  state[0] = MixColumn(state[0]);
+  state[1] = MixColumn(state[1]);
+  state[2] = MixColumn(state[2]);
+  state[3] = MixColumn(state[3]);
 }
 
 void KeyExpansion(unsigned char *key, unsigned int *w, AESparam *aesParam)
@@ -50,14 +105,6 @@ void KeyExpansion(unsigned char *key, unsigned int *w, AESparam *aesParam)
   }
 }
 
-void AddRoundKey(unsigned int *state, unsigned int *w)
-{
-  state[0] ^= w[0];
-  state[1] ^= w[1];
-  state[2] ^= w[2];
-  state[3] ^= w[3];
-}
-
 void Cipher(unsigned char *in, unsigned char *out, unsigned int *w, AESparam *aesParam)
 {
   unsigned int state[4];
@@ -72,12 +119,37 @@ void Cipher(unsigned char *in, unsigned char *out, unsigned int *w, AESparam *ae
 
   AddRoundKey(state, w);
 
+  for(int i = 1; i < aesParam->Nr; i++)
+  {
+    //SubBytes
+    state[0] = SubWord(state[0]);
+    state[1] = SubWord(state[1]);
+    state[2] = SubWord(state[2]);
+    state[3] = SubWord(state[3]);
+    //ShiftRows
+    ShiftRows(state);
+    //MixColumns
+    MixColumns(state);
+    //AddRoundKey
+    AddRoundKey(state, w + 4 * i);
+  }
+
+  //SubBytes
+  state[0] = SubWord(state[0]);
+  state[1] = SubWord(state[1]);
+  state[2] = SubWord(state[2]);
+  state[3] = SubWord(state[3]);
+  //ShiftRows
+  ShiftRows(state);
+  //AddRoundKey
+  AddRoundKey(state, w + 4 * aesParam->Nr);
+
   for(size_t i = 0; i < aesParam->Nb; i++)
   {
-    out[i * 4] = state[i] >> 24 & 0x000000FFU;
-    out[i * 4 + 1] = state[i] >> 16 & 0x000000FFU;
-    out[i * 4 + 2] = state[i] >> 8 & 0x000000FFU;
-    out[i * 4 + 3] = state[i] & 0x000000FFU;
+    out[i * 4] = BYTE3(state[i]);
+    out[i * 4 + 1] = BYTE2(state[i]);
+    out[i * 4 + 2] = BYTE1(state[i]);
+    out[i * 4 + 3] = BYTE0(state[i]);
   }
 }
 
@@ -97,11 +169,24 @@ int main()
 
   Cipher(cipherInput, cipherOutput, w ,selectedAesParam);
 
+  int failedBytes = 0;
   for(size_t i = 0; i < 4 * selectedAesParam->Nb; i++)
   {
+    if(cipherOutput[i] != cipherOutput128[i])
+    {
+      failedBytes++;
+    }
     printf("%02x", cipherOutput[i]);
   }
   printf("\n");
+  if(failedBytes)
+  {
+    printf("Total bytes failed: %d\n", failedBytes);
+  }
+  else
+  {
+    printf("Cipher OK! YAY!\n");
+  }
 
   return 0;
 }
